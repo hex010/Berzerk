2b3d2549e672391e451468fbd3822552093a8542
Robertas Nevoinas <robertas.nevoinas@stud.vilniustech.lt>
2022-11-08 00:14:21 +0200

    Win langas

--- Berzerk/src/Main.java
+++ Berzerk/src/Main.java
@@ -1,5 +1,18 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Berzerk game by Robertas");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
