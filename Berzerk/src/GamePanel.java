95c356d39448e4a73399752c8c98090b597d1102
Robertas Nevoinas <robertas.nevoinas@stud.vilniustech.lt>
2022-11-08 01:06:16 +0200

    update, paint metodai. Thread & FPS.

--- Berzerk/src/GamePanel.java
+++ Berzerk/src/GamePanel.java
@@ -1,15 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16;
     final int scale = 3;
     final int tileSize = originalTileSize * scale;
-
     final int maxScreenColumn = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenColumn;
     final int screenHeight = tileSize * maxScreenRow;
+    int fpsCount = 60;
+    int oneSecondInNanoTime = 1000000000;
+
+    Thread gameThread;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -17,4 +20,36 @@ public class GamePanel extends JPanel {
         this.setDoubleBuffered(true);
     }
 
+    @Override
+    public void run() {
+        double drawInterval = oneSecondInNanoTime / fpsCount;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null){
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if(delta >= 1){
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    public void update(){
+
+    }
+
+    public void paint(Graphics g){
+        super.paint(g);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
 }
